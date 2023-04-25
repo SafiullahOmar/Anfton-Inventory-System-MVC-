1151fbc63399feafc3366181e8dd13969a0a560e
Safiullah Omar <omar.safiullah@gmail.com>
2023-04-25 18:21:52 +0430

    Saveing Category by Jax Call

--- Controllers/MasterController.cs
+++ Controllers/MasterController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Inventory_Anfton.BusinessLogic.IServices;
+using Inventory_Anfton.BusinessLogic.ServiceCls;
+using Inventory_Anfton.Utilites.RequestCls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,22 @@ namespace Inventory_Anfton.Controllers
 {
     public class MasterController : Controller
     {
+        IMaster _master;
+        public MasterController()
+        {
+            _master = new MasterCls();
+        }
         // GET: Master
         public ActionResult Category()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult AddCategory(RequestCls obj) {
+            var result=_master.AddCategory(obj);
+
+            return Json(result,JsonRequestBehavior.AllowGet);
         }
     }
 }
